a75410ced07eaf6e600b8077e3a01770d5d7a726
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-12-05 11:50:02 +0530

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowingController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index'])->name('books.index');
+
+Route::resource('books', BookController::class)->except(['show']);
+
+// Borrowing endpoints (form submissions)
+Route::post('/borrow/issue', [BorrowingController::class, 'issue'])->name('borrow.issue');
+Route::post('/borrow/return', [BorrowingController::class, 'returnBook'])->name('borrow.return');
